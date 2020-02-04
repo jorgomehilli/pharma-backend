64ee34e70259b4f9391ca084110ec2cf8de62ead
MEHILLI <jorgo.mehilli@lhind.dlh.de>
2020-02-04 15:02:26 +0100

    Logging users from DB

--- src/main/java/de/dlh/lhind/pharma/repository/UserRepository.java
+++ src/main/java/de/dlh/lhind/pharma/repository/UserRepository.java
@@ -2,8 +2,14 @@ package de.dlh.lhind.pharma.repository;
 
 import de.dlh.lhind.pharma.models.User;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 public interface UserRepository extends JpaRepository<User, Long> {
+
+    @Query(value = "SELECT * FROM users  WHERE email = :email AND to_date IS NULL",
+            nativeQuery = true)
+    User findByEmail(@Param("email") String email);
 
 
 }
--- src/main/java/de/dlh/lhind/pharma/service/MyUserDetailsService.java
+++ src/main/java/de/dlh/lhind/pharma/service/MyUserDetailsService.java
@@ -1,5 +1,10 @@
 package de.dlh.lhind.pharma.service;
 
+import de.dlh.lhind.pharma.models.Roles;
+import de.dlh.lhind.pharma.repository.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
 import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
@@ -7,11 +12,32 @@ import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.stereotype.Service;
 
 import java.util.ArrayList;
+import java.util.Collection;
+import java.util.stream.Collectors;
 
 @Service
 public class MyUserDetailsService implements UserDetailsService {
+
+    @Autowired
+    private UserRepository userRepository;
+
     @Override
     public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
-        return new User("foo", "foo", new ArrayList<>());
+
+        de.dlh.lhind.pharma.models.User user = userRepository.findByEmail(username);
+
+        if(user == null) {
+            throw new UsernameNotFoundException("Invalid username or password!");
+        }
+        return new org.springframework.security.core.userdetails.User(user.getEmail(),
+                user.getPassword(),
+                mapRolesToAuthorities(user.getRoles()));
+
+       // return new User("foo@foo.com", "12345678", new ArrayList<>());
+    }
+
+    private Collection<? extends GrantedAuthority> mapRolesToAuthorities(Collection<Roles> roles) {
+        return roles.stream().map(role -> new SimpleGrantedAuthority(role.getName()))
+                .collect(Collectors.toList());
     }
 }
